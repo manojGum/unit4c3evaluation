8bfa48a9ad60fac0eb529cd0879221b3f8bcd263
manojGum <Manojgum@gmail.com>
2022-03-28 10:50:58 +0530

    Update book.model.js

--- models/book.model.js
+++ models/book.model.js
@@ -9,3 +9,23 @@ const bookSchema=new mongoose.Schema({
 })
 const book=mongoose.model("book",bookSchema);
 module.exports=book
+
+
+// const userSchema = new mongoose.Schema({
+//     first_name:{type: String, required: true},
+//     last_name:{type: String, required: false},
+//     email:{type: String, required:true,unique:true},
+//     pincode:{type: String, required:true}, 
+//     age:{type: Number, required:true}, 
+//     gender: {
+//           type: String,
+//           enum: ["Male", "Female"],
+//           default: "Male",
+//         }, 
+       
+//     }, 
+//     {
+//         versionKey: false,
+//         timestamps: true,
+//       })
+    
